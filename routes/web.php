095e82027cdc2898897f816d428dc22de75025f2
tongyuan <3105609687@qq.com>
2021-11-18 21:02:47 +0800

    秒杀商品详情页

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('installments/{installment}', 'InstallmentsController@show')->name('installments.show');
     Route::get('installments/{installment}/alipay', 'InstallmentsController@payByAlipay')->name('installments.alipay');
     Route::get('installments/alipay/return', 'InstallmentsController@alipayReturn')->name('installments.alipay.return');
+    Route::post('seckill_orders', 'OrdersController@seckill')->name('seckill_orders.store');
 });
 // 后端回调不能放在 auth 中间件中
 Route::post('installments/alipay/notify', 'InstallmentsController@alipayNotify')->name('installments.alipay.notify');
